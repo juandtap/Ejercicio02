159b51ada7d265e080388fcbc901ea5452cefdf6
juandtap <jtapiav4@est.ups.edu.ec>
2022-05-04 16:00:54 -0500

    terminado interfaces

--- src/main/java/Modelo/Pelicula.java
+++ src/main/java/Modelo/Pelicula.java
@@ -4,10 +4,84 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author diego
  */
 public class Pelicula {
+    private String nombre;
+    private String produccion;
+    private String director;
+    private String fechaEstreno;
+    private double duracion;
+    private ArrayList<Personaje> listaPersonajes;
+
+    public Pelicula(String nombre, String produccion, String director, String fechaEstreno, double duracion) {
+        this.nombre = nombre;
+        this.produccion = produccion;
+        this.director = director;
+        this.fechaEstreno = fechaEstreno;
+        this.duracion = duracion;
+        this.listaPersonajes = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula: \nnombre=" + nombre + "\nproduccion=" + produccion + "\ndirector=" + director + "\nfechaEstreno=" + fechaEstreno + "\nduracion=" + duracion + "\nLista de personajes: \n" + listaPersonajes;
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProduccion() {
+        return produccion;
+    }
+
+    public void setProduccion(String produccion) {
+        this.produccion = produccion;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(String fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+
+    public ArrayList<Personaje> getListaPersonajes() {
+        return listaPersonajes;
+    }
+
+    public void setListaPersonajes(ArrayList<Personaje> listaPersonajes) {
+        this.listaPersonajes = listaPersonajes;
+    }
+    
+    
     
 }
--- src/main/java/Modelo/Personaje.java
+++ src/main/java/Modelo/Personaje.java
@@ -9,5 +9,68 @@ package Modelo;
  * @author diego
  */
 public class Personaje {
+    private String nombre;
+    private String genero;
+    private String actor;
+    private int edad;
+    private double estatura;
+
+    public Personaje(String nombre, String genero, String actor, int edad, double estatura) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.actor = actor;
+        this.edad = edad;
+        this.estatura = estatura;
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje: \nnombre: " + nombre + "\ngenero" + genero + "\nactor: " + actor + "\nedad: " + edad + "\nestatura: " + estatura + '}';
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+    
+    
+    
     
 }
